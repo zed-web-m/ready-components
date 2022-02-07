b749d40705a6e8c3d669c2529d5dd9133258769d
zed-web-m <72280188+zed-web-m@users.noreply.github.com>
2022-02-07 11:40:08 +0200

    add: light mode theme

--- js/common.js
+++ js/common.js
@@ -121,4 +121,30 @@ document.addEventListener('DOMContentLoaded', function () {
   btnToUp.addEventListener('click', () => {
     window.scrollTo({ top: 0, behavior: 'smooth' })
   })
+
+  // light mode
+  const toggleSwitch = document.querySelector(
+    '.theme-switch-wrapper input[type="checkbox"]'
+  )
+  const currentTheme = localStorage.getItem('theme')
+
+  if (currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme)
+
+    if (currentTheme === 'light') {
+      toggleSwitch.checked = true
+    }
+  }
+
+  function switchTheme(e) {
+    if (e.target.checked) {
+      document.documentElement.setAttribute('data-theme', 'light')
+      localStorage.setItem('theme', 'light')
+    } else {
+      document.documentElement.setAttribute('data-theme', 'dark')
+      localStorage.setItem('theme', 'dark')
+    }
+  }
+
+  toggleSwitch.addEventListener('change', switchTheme, false)
 })
